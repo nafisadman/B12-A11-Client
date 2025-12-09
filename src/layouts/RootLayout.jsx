54015605d96bfd01078dc1eb5050b44e20ac2426
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-09 14:03:34 +0600

    Added Navbar and Footer Components

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const RootLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
