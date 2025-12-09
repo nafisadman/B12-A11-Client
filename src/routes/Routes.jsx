2e5f8f23cd38cf4ea7f55ce3744049498d5e8078
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-09 10:51:57 +0600

    Set up Home Layout

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <RootLayout></RootLayout>,
+    children: [
+        {
+            path: "/",
+            Component: Home
+        }
+    ]
   },
 ]);
 
