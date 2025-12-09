95af73e40ef4d67e9c58e72349e303e77827c331
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-09 13:48:05 +0600

    Added frontend for Auth Pages

--- src/pages/Auth/Registration.jsx
+++ src/pages/Auth/Registration.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Login = () => {
+const Registration = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
+          <h1 className="text-5xl font-bold">Register now!</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
@@ -22,7 +22,7 @@ const Login = () => {
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
-              <button className="btn btn-neutral mt-4">Login</button>
+              <button className="btn btn-neutral mt-4">Register</button>
             </fieldset>
           </div>
         </div>
@@ -31,4 +31,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Registration;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Auth/Login";
+import Registration from "../pages/Auth/Registration";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,20 @@ const router = createBrowserRouter([
         }
     ]
   },
+  {
+    path: "/auth",
+    element: <RootLayout></RootLayout>,
+    children: [
+      {
+        path: "/auth/login",
+        Component: Login
+      },
+      {
+        path: "/auth/registration",
+        Component: Registration
+      }
+    ]
+  }
 ]);
 
 export default router;
