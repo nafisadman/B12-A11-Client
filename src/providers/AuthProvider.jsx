b8b614a6ef8644c83d66ff2ee8f98837551483af
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-09 13:37:33 +0600

    Wrapped RouterProvider within AuthProvider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import { createContext } from "react";
+import app from "../firebase/firebase.config";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-const AuthProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  return <AuthContext>{children}</AuthContext>;
 };
 
 export default AuthProvider;
