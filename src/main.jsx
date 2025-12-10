4819ee088b57f596af2e553f9138e4eff9927eb2
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-10 09:35:56 +0600

    imgbb integrated in the registration form with loading status using react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from './App.jsx'
 import router from './routes/Routes.jsx'
 import { RouterProvider } from 'react-router'
 import AuthProvider from './providers/AuthProvider.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
+      <Toaster />
     </AuthProvider>
   </StrictMode>,
 )
--- src/pages/Auth/Registration.jsx
+++ src/pages/Auth/Registration.jsx
@@ -1,6 +1,46 @@
+import axios from "axios";
 import React from "react";
+import toast from "react-hot-toast";
+import useTitle from "../../hooks/useTitle";
 
 const Registration = () => {
+  useTitle("Registration");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const photo = form.photo.files[0];
+    const password = form.password.value;
+
+    console.log(
+      "\nEmail: ",
+      email,
+      "\nPhoto: ",
+      photo,
+      "\nPassword: ",
+      password
+    );
+
+    // Photo
+    const imgbbUploadPromise = axios.post(
+      `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_KEY}`,
+      { image: photo },
+      { headers: { "Content-Type": "multipart/form-data" } }
+    );
+
+    try {
+      const imgbbResponse = await toast.promise(imgbbUploadPromise, {
+        loading: "Uploading image...",
+        success: <b>Image uploaded successfully!</b>,
+        error: <b>Failed to upload image.</b>,
+      });
+      console.log(imgbbResponse.data.data.display_url);
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -14,16 +54,35 @@ const Registration = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <fieldset className="fieldset">
+            <form onSubmit={handleRegister} className="fieldset">
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+                required
+              />
+              <label className="label">Photo</label>
+              <input
+                name="photo"
+                type="file"
+                className="file-input"
+                placeholder="Photo"
+              />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+                required
+              />
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
               <button className="btn btn-neutral mt-4">Register</button>
-            </fieldset>
+            </form>
           </div>
         </div>
       </div>
