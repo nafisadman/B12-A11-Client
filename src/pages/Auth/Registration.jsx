7811b165fa278daf43e86de08d082b5958e1f5bd
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-10 10:49:05 +0600

    User can register with Google

--- src/pages/Auth/Registration.jsx
+++ src/pages/Auth/Registration.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
-import React from "react";
+import React, { use } from "react";
 import toast from "react-hot-toast";
 import useTitle from "../../hooks/useTitle";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Navigate } from "react-router";
 
 const Registration = () => {
   useTitle("Registration");
+
+  const { user, setUser, signInWithGoogle } = use(AuthContext);
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -41,6 +45,19 @@ const Registration = () => {
     }
     return;
   };
+
+  const handleRegisterWithGoogle = () => {
+    console.log("Registered with Google Successfully");
+    signInWithGoogle()
+      .then((result) => {
+        console.log(result.user);
+        Navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -54,6 +71,40 @@ const Registration = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
+            {/* Google */}
+            <button
+              onClick={handleRegisterWithGoogle}
+              className="btn bg-white text-black border-[#e5e5e5]"
+            >
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              Register with Google
+            </button>
             <form onSubmit={handleRegister} className="fieldset">
               <label className="label">Email</label>
               <input
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,14 +1,24 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import app from "../firebase/firebase.config";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 const auth = getAuth(app);
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  return <AuthContext>{children}</AuthContext>;
+  const [user, setUser] = useState(null);
+  const signInWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const authData = {
+    user,
+    setUser,
+    signInWithGoogle,
+  };
+  return <AuthContext value={authData}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
